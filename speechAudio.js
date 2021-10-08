d1f5f26ae56fe65f1eb75ce47b65f744c4feaf9c
danuja-wije <danujawijerathne45@gmail.com>
2021-10-08 17:06:40 +0530

    make directories

--- speechAudio.js
+++
@@ -1,6 +0,0 @@
-const speak = (msg) =>{
-    const sp = new SpeechSynthesisUtterance(msg);
-    [sp.voice] = speechSynthesis.getVoice();
-    speechSynthesis.speak(sp);
-};
-speak('My name is Inigo montoya,'+'you killed my father,'+'prepare to die!');
